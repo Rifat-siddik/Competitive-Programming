036d366d4467805ef422d51305ebbac22ba960ef
Abu Bakkar Siddik Rifat <siddikcserifat@gmail.com>
2026-03-08 00:13:29 +0600

    cmath_bibrary practices

---
+++ Cpp/cmath_library.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+ 
+int main(){
+  cout<<max(3,5)<<endl;
+  cout<<min(34,3)<<endl;
+  cout<<sqrt(16)<<endl;
+  cout<<round(4.6)<<endl;
+  cout<<log(2);
+}
