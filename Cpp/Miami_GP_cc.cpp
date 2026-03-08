db62b361cadf09aa9820c9ac08a7a9f1d4e20271
Abu Bakkar Siddik Rifat <siddikcserifat@gmail.com>
2026-03-09 02:52:04 +0600

    codechef basic problem Miami GP

---
+++ Cpp/Miami_GP_cc.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int T;cin>>T;
+    while (T--)
+    {
+        int x,y;
+        cin>>x>>y;
+        if(y*100<=x*107){
+            cout<<"Yes"<<endl;
+        }else{
+            cout<<"no"<<endl;
+        }
+    }
+    
+}
