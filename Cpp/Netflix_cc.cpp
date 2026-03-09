c7930d5af72d86fd5879bce5064dde4ed127a855
Abu Bakkar Siddik Rifat <siddikcserifat@gmail.com>
2026-03-09 23:48:21 +0600

    codechef basic problem slove name Netflix

---
+++ Cpp/Netflix_cc.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;cin>>t;
+    while(t--){
+        int a,b,c,x;
+        cin>>a>>b>>c>>x;
+        if((a+b)>=x || (b+c)>=x ||(a + c) >= x){
+            cout<<"yes"<<endl;
+        }else{
+            cout<<"no"<<endl;
+        }
+    }
+}
